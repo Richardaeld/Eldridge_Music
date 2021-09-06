18828aa04ab89cb7b2b462ee4a8eec9164c443b6
Richardaeld <Richardaeld@gmail.com>
2021-09-06 14:57:44 -0400

    Removed unused code from borderjs

--- static/js/borderjs.js
+++ static/js/borderjs.js
@@ -1,16 +1,3 @@
-
-
-//---- Set piano top (lid) height and width
-// Find and alter header height/width
-var bodyWidth = document.body.offsetWidth
-var bodyHeight = document.body.offsetHeight
-bodyWidth = bodyWidth * .275
-// Set piano top height and width
-// document.getElementsByClassName("piano-top")[0].style.borderBottomWidth = bodyHeight + "px"
-// document.getElementsByClassName("piano-top")[0].style.borderLeftWidth = bodyWidth + "px"
-
-
-
 //Set depression effect on .key-white with adjacent .key-black
 var findKeyWhite = document.querySelectorAll(".key-white");
 findKeyWhite.forEach(keyWhiteEvent);
@@ -18,20 +5,20 @@ function keyWhiteEvent(keyWhite, keyWhiteIndex) {
 
     keyWhite.addEventListener('mouseover', function() {
         if(keyWhite.nextSibling.nextSibling != null && keyWhite.nextSibling.nextSibling.classList.contains("key-black")){
-            keyWhite.nextSibling.nextSibling.classList.add("key-white-depressed-bottom")
+            keyWhite.nextSibling.nextSibling.classList.add("key-white-depressed-bottom");
         }
         if(keyWhite.previousSibling.previousSibling != null && keyWhite.previousSibling.previousSibling.classList.contains("key-black")){
-            keyWhite.previousSibling.previousSibling.classList.add("key-white-depressed-top")
+            keyWhite.previousSibling.previousSibling.classList.add("key-white-depressed-top");
         }
-    })
+    });
 
     keyWhite.addEventListener('mouseout', function() {
         if(keyWhite.nextSibling.nextSibling != null && keyWhite.nextSibling.nextSibling.classList.contains("key-black")){
-            keyWhite.nextSibling.nextSibling.classList.remove("key-white-depressed-bottom")
+            keyWhite.nextSibling.nextSibling.classList.remove("key-white-depressed-bottom");
         }
         if(keyWhite.previousSibling.previousSibling != null && keyWhite.previousSibling.previousSibling.classList.contains("key-black")){
-            keyWhite.previousSibling.previousSibling.classList.remove("key-white-depressed-top")
+            keyWhite.previousSibling.previousSibling.classList.remove("key-white-depressed-top");
         }
-    })
+    });
 
 }
