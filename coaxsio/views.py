ec7eb947c7d80b9e988306785cc85be38a3d31d7
Richardaeld <Richardaeld@gmail.com>
2021-09-07 12:30:44 -0400

    Added functionality to coaxsio app

--- coaxsio/views.py
+++ coaxsio/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def coaxsio(request):
+    """ This view is for the online piano the coaxsio """
+    return render(request, 'coaxsio/coaxsio.html')
