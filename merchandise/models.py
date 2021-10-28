4bdf08f355953882cc885caebeb568ed0532efeb
Richardaeld <Richardaeld@gmail.com>
2021-10-28 17:43:29 -0400

    Improved models

--- merchandise/models.py
+++ merchandise/models.py
@@ -2,15 +2,6 @@ from django.db import models
 from django.db.models.fields import DateField
 
 
-class Image(models.Model):
-    name = models.CharField(max_length=254)
-    image = models.ImageField()
-    image_url = models.CharField(max_length=1024, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Music_Style(models.Model):
     style = models.CharField(max_length=100)
 
@@ -21,15 +12,16 @@ class Music_Style(models.Model):
 class Merch(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True, unique=True)
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
-    image = models.ForeignKey('Image', null=True, blank=True, on_delete=models.SET_NULL)
     author = models.CharField(max_length=100, null=True, blank=True)
-    release_date = DateField(auto_now=False)
-    description = models.TextField()
+    style = models.ForeignKey(Music_Style, null=True, blank=True, on_delete=models.SET_NULL)
+    release_date = DateField(auto_now=False, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     active_product = models.BooleanField(default=True)
     stocked = models.BooleanField(default=True)
-
+    rating = models.DecimalField(max_digits=8, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.CharField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
