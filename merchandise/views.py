b5e320cfe1f85c7fa7b6bc3abcb4935e7e2616f4
Richardaeld <Richardaeld@gmail.com>
2021-10-28 21:09:41 -0400

    Created a base merchandise page to display merch with basic functionality (minus anchors) and added css file for merch

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Merch
 
 def merchandise(request):
 
+    merch = Merch.objects.all()
+
     template = 'merchandise/merchandise.html'
 
-    context = {}
+    context = {
+        'merchandise': merch,
+    }
 
     return render(request, template, context)
