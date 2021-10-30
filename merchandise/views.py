7f83f62fed050d1b50de5e421bde93390287fbd6
Richardaeld <Richardaeld@gmail.com>
2021-10-30 19:57:53 -0400

    removed lessons from search bar and added merchandise in its place

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Merch
 
 def merchandise(request):
 
     merch = Merch.objects.all()
+
+    # Used by user search bar
+    if request.GET:
+        # if 'instrument' in request.GET:
+        #     instruments = request.GET['instrument']
+        #     merch = merch.filter(merchandise__name=(Merchandise))
+
+        if 'userquery' in request.GET:
+            query = request.GET['userquery']
+            if not query:
+                messages.info(request, "No search information input!")
+                return redirect(reverse('merchandise'))
+
+            queries = Q(name__icontains=query) | Q(author__icontains=query)
+            merch = merch.filter(queries)
+
 
     template = 'merchandise/merchandise.html'
 
