e520a631de1e36aa23de767bc1edffea2459cbef
Richardaeld <Richardaeld@gmail.com>
2021-10-31 16:35:04 -0400

    Created contexts.py and connected to entire app for shopping_bag contents

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -5,6 +5,4 @@ def shopping_bag(request):
 
     template = 'shopping_bag/shopping_bag.html'
 
-    context = {}
-
-    return render(request, template, context)
+    return render(request, template)
