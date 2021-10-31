b8911f9fb530b11af65ddc865df957266efcfbf1
Richardaeld <Richardaeld@gmail.com>
2021-10-31 16:57:42 -0400

    Created additional views in shopping_bag to allow users to add items to their 'shopping_bag'

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shopping_bag, name='shopping_bag'),
+    path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def shopping_bag(request):
@@ -6,3 +6,18 @@ def shopping_bag(request):
     template = 'shopping_bag/shopping_bag.html'
 
     return render(request, template)
+
+def add_to_cart(request, item_id):
+
+    amount = int(request.POST.get('amount'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += amount
+    else:
+        cart[item_id] = amount
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
