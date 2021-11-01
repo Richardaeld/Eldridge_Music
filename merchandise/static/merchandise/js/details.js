0706c7304d62dfeb105c761e12b4cf9661b64460
Richardaeld <Richardaeld@gmail.com>
2021-10-31 22:39:37 -0400

    Improved UX, cleaned up code, improved page layout, added comments

--- merchandise/static/merchandise/js/details.js
+++ merchandise/static/merchandise/js/details.js
@@ -1,5 +1,5 @@
 // Extends card and image height to proper proportions to negate use of scroll bar
-let contentHeight = document.getElementById("merch-form").offsetHeight;
+let contentHeight = document.getElementById("card-body").offsetHeight;
 let cardHeight = document.getElementById("card").offsetHeight;
 if (contentHeight > cardHeight) {
     document.getElementsByClassName("merch-card")[0].style.height = contentHeight + "px"
@@ -8,20 +8,20 @@ if (contentHeight > cardHeight) {
 
 // Removes one from bag total, to a min of 0
 document.getElementById("remove-button").addEventListener("click", function() {
-    let bag = document.getElementById("in-bag").getElementsByTagName("span")[0];
-    let bagContent = parseInt(bag.textContent);
+    let bag = document.getElementById("in-bag").getElementsByTagName("input")[0];
+    let bagContent = parseInt(bag.value);
     if (bagContent > 0) {
         bagContent = bagContent - 1;
-        bag.textContent = bagContent;
+        bag.value = bagContent;
     }
 })
 
 // Adds one to bag total, to a max of 99
 document.getElementById("add-button").addEventListener("click", function() {
-    let bag = document.getElementById("in-bag").getElementsByTagName("span")[0];
-    let bagContent = parseInt(bag.textContent);
+    let bag = document.getElementById("in-bag").getElementsByTagName("input")[0];
+    let bagContent = parseInt(bag.value);
     if (bagContent < 99) {
         bagContent = bagContent + 1;
-        bag.textContent = bagContent;
+        bag.value = bagContent;
     }
 })
