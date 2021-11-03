752c7543d4250f35fa6b1de5e99fe6b8a108fe24
Richardaeld <Richardaeld@gmail.com>
2021-11-03 17:20:59 -0400

    numerous syntax and name corrections

--- invoice/admin.py
+++ invoice/admin.py
@@ -3,20 +3,25 @@ from .models import Invoice, InvoiceLineItem
 
 
 class InvoiceLineItemAdminInLine(admin.TabularInline):
-    inlines = InvoiceLineItem
+    model = InvoiceLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class InvoiceAdmin(admin.ModelAdmin):
     inlines = (InvoiceLineItemAdminInLine,)
 
-    readonly_fields = ('invoice_number', 'date', 'delivery_total', 'order_total', 'grand_total',)
+    readonly_fields = ('invoice_number', 'date_time', 'delivery_total',
+                        'invoice_total', 'grand_total',)
 
-    fields = ('invoice_number', 'date', 'name', 'email', 'phone', 'country', 'post_code', 'state_country', 'street_address_billing', 'street_address_shipping', 'city', 'delivery_total', 'invoice_total', 'grand_total',)
+    fields = ('invoice_number', 'date_time', 'name', 'email', 'phone',
+            'country', 'post_code', 'state_country', 'street_address_billing',
+            'street_address_shipping', 'city', 'delivery_total',
+            'invoice_total', 'grand_total',)
 
-    list_display = ('invoice_number', 'date', 'name', 'invoice_total', 'delivery_total', 'grand_total',)
+    list_display = ('invoice_number', 'date_time', 'name', 'invoice_total',
+                    'delivery_total', 'grand_total',)
 
-    ordering = ('-date')
+    ordering = ('-date_time',)
 
 
 admin.site.register(Invoice, InvoiceAdmin)
