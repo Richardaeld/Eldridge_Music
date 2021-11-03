a50814de6256997dcf6f8f79c287e74a5b04acf2
Richardaeld <Richardaeld@gmail.com>
2021-11-02 20:55:42 -0400

    Created two new views to handle quantity change and item removal from user's cart

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
 from merchandise.models import Merch
 
@@ -26,3 +26,37 @@ def add_to_cart(request, item_id):
     messages.success(request, item.name + " was added to cart")
 
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+
+    amount = int(request.POST.get('amount'))
+    cart = request.session.get('cart', {})
+
+    if amount > 0:
+        cart[item_id] = amount
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+
+    # item  = get_object_or_404(Merch, pk=item_id)
+
+    # messages.success(request, item.name + " was added to cart")
+
+    return redirect(reverse('shopping_bag'))
+
+
+def remove_cart_item(request, item_id):
+
+    cart = request.session.get('cart', {})
+
+    cart.pop(item_id)
+
+    request.session['cart'] = cart
+
+    # item  = get_object_or_404(Merch, pk=item_id)
+
+    # messages.success(request, item.name + " was added to cart")
+
+    return redirect(reverse('shopping_bag'))
