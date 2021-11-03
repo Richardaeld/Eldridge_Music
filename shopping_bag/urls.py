4093111fad182e2a28b988dcd8fc1b09f8a7ba50
Richardaeld <Richardaeld@gmail.com>
2021-11-02 20:56:42 -0400

    Created two new urls to handle quantity change and item removal from user's cart

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.shopping_bag, name='shopping_bag'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
+    path('adjust_cart/<item_id>', views.adjust_cart, name='adjust_cart'),
+    path('remove_cart_item/<item_id>', views.remove_cart_item, name='remove_cart_item')
 ]
