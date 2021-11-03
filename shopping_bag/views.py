4d2fe4dd827681ce54f3966ef783f2bbab624345
Richardaeld <Richardaeld@gmail.com>
2021-11-03 16:00:28 -0400

    Beautified code

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -13,50 +13,46 @@ def add_to_cart(request, item_id):
     amount = int(request.POST.get('amount'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
+    item  = get_object_or_404(Merch, pk=item_id)
 
     if item_id in list(cart.keys()):
         cart[item_id] += amount
+        messages.success(request, f'{item.name}\'s quantity is now {cart[item_id]}')
     else:
         cart[item_id] = amount
+        messages.success(request, f'({amount}) - {item.name} added to cart')
 
     request.session['cart'] = cart
-
-    item  = get_object_or_404(Merch, pk=item_id)
-
-    messages.success(request, item.name + " was added to cart")
 
     return redirect(redirect_url)
 
 
 def adjust_cart(request, item_id):
 
+    item  = get_object_or_404(Merch, pk=item_id)
     amount = int(request.POST.get('amount'))
     cart = request.session.get('cart', {})
 
     if amount > 0:
+        print(amount)
         cart[item_id] = amount
     else:
         cart.pop(item_id)
 
     request.session['cart'] = cart
-
-    # item  = get_object_or_404(Merch, pk=item_id)
-
-    # messages.success(request, item.name + " was added to cart")
+    messages.success(request, f'{item.name}\'s quantity is now {amount}')
 
     return redirect(reverse('shopping_bag'))
 
 
 def remove_cart_item(request, item_id):
 
-    cart = request.session.get('cart', {})
-
-    cart.pop(item_id)
-
-    request.session['cart'] = cart
-
-    # item  = get_object_or_404(Merch, pk=item_id)
-
-    # messages.success(request, item.name + " was added to cart")
-
+    try:
+        item  = get_object_or_404(Merch, pk=item_id)
+        cart = request.session.get('cart', {})
+        cart.pop(item_id)
+        request.session['cart'] = cart
+        messages.success(request, f'{item.name} was removed from cart')
+    except Exception as e:
+        messages.error(request, f'Error occured: {e} while removing {item.name} try again later')
     return redirect(reverse('shopping_bag'))
