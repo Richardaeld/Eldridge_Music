ac84b3437ae1f2fa282167212e73d4f47864ef53
Richardaeld <Richardaeld@gmail.com>
2021-11-04 19:52:12 -0400

    Upated webhooks to test on live site

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('', views.invoice, name='invoice'),
-    path('checkout_success/<invoice_number>', views.checkout_success, name='checkout_success')
+    path('checkout_success/<invoice_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook')
     # path('special/<special_id>/<lesson_subscription>', views.checkout_special, name='checkout_special'),
     # path('lesson/<lesson_id>/<lesson_sku>', views.checkout_lesson, name='checkout_lesson'),
     # path('lesson-chechout/<sku>/<item_pk>', views.record_invoice, name='record_invoice')
--- invoice/webhook_handler.py
+++ invoice/webhook_handler.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 
-class StripeWH_Hndler:
+class StripeWH_Handler:
     """
     Controls webhooks from Stripe
     """
@@ -12,6 +12,24 @@ class StripeWH_Hndler:
     def handle_event(self,event):
         """
         Contorls generic/unknown/unexpected webhook
+        """
+        return HttpResponse(
+            content=f'Generic/Unknown/Unexpected webhook received: {event["type"]}',
+            status=200
+            )
+
+    def handle_payment_intent_succeeded(self,event):
+        """
+        Contorls payment_intent.succeeded webhook
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+            )
+
+    def handle_payment_intent_payment_failed(self,event):
+        """
+        Contorls payement_intent.payment_failed webhook
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
