33ecb4c33c4b668122039426451b7aeec97c9574
Richardaeld <Richardaeld@gmail.com>
2021-11-05 17:18:09 -0400

    Updated to work with redesign (removal of lessons app)

---
+++ profile_history/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from .models import User_Profile_History
+
+
+class User_Profile_History_Form(forms.ModelForm):
+    class Meta:
+        model = User_Profile_History
+        # fields = ('default_full_name', 'default_email', 'default_phone', 'default_street_address', 'default_city', 'default_state_county', 'default_post_code', 'default_country',)
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_full_name': 'Full Name',
+            'default_email': 'Email Address',
+            'default_phone': 'Phone Number',
+            'default_city': 'City',
+            'default_street_address_billing': 'Billing Street Address',
+            'default_street_address_shipping': 'Shipping Street Address',
+            'default_state_county': 'State',
+            'default_post_code': 'Post Code',
+            'default_country': 'Country',
+        }
+
+        self.fields['default_full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    self.fields[field].required = False
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
--- profile_history/views.py
+++ profile_history/views.py
@@ -1,26 +1,66 @@
 from django.shortcuts import get_object_or_404, render
 from .models import User_Profile_History
 from invoice.forms import InvoiceForm
+from django.contrib import messages
+from .forms import User_Profile_History_Form
+
 
 def profile(request):
 
-    user = get_object_or_404(User_Profile_History, user=request.user)
-    invoice = InvoiceForm(initial={
-        'name': user.default_full_name,
-        'email': user.default_email,
-        'phone': user.default_phone,
-        'pronouns': user.default_pronouns,
-        'city': user.default_city,
-        'street_address_billing': user.default_street_address,
-        'state_county': user.default_state_county,
-        'post_code': user.default_post_code,
-        'country': user.default_country,
-    })
+    userProfile = get_object_or_404(User_Profile_History, user=request.user)
+    
+    form = User_Profile_History_Form(instance=userProfile)
+    invoice = userProfile.invoices.all()
+
+    if request.method == 'POST':
+        form = User_Profile_History_Form(request.POST, instance=userProfile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile has been updated")
+
+    # invoice = InvoiceForm(initial={
+    #     'name': userProfile.default_full_name,
+    #     'email': userProfile.default_email,
+    #     'phone': userProfile.default_phone,
+    #     # 'pronouns': userProfile.default_pronouns,
+    #     'city': userProfile.default_city,
+    #     'street_address_billing': userProfile.default_street_address_billing,
+    #     'street_address_shipping': userProfile.default_street_address_shipping,
+    #     'state_county': userProfile.default_state_county,
+    #     'post_code': userProfile.default_post_code,
+    #     'country': userProfile.default_country,
+    # })
+
+    # if request.method == 'POST':
+    #     update_user_info = User_Profile_History_Form(request.POST, instance=userProfile)
+    #     if update_user_info.is_valid():
+    #         update_user_info.save()
+    #         messages.success(request, "Profile has been updated")
+    #     else:
+    #         messages.error(request, "An error occurred please try again later")
+        # user = get_object_or_404(User_Profile_History, user=request.user)
+        # update_user = {
+        # 'default_full_name': request.POST['name'],
+        # 'default_email': request.POST['email'],
+        # 'default_phone': request.POST['phone'],
+        # # 'default_pronouns': request.POST['default_pronouns'],
+        # 'default_city': request.POST['city'],
+        # 'default_street_address': request.POST['street_address'],
+        # 'default_state_county': request.POST['state_county'],
+        # 'default_post_code': request.POST['post_code'],
+        # 'default_country': request.POST['country'],
+        # }
 
 
     context = {
-        'user': user,
+        'form': form,
         'invoice': invoice,
+        'on_profile_history_page': True
+
+
+        # 'user': userProfile,
+        # 'invoice': invoice,
+        # 'update_user_info': update_user_info,
     }
 
     return render(request, 'profile_history/profile.html', context)
