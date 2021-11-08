83dc8758c7eb629145b477b9bfe386205c5dbe49
Richardaeld <Richardaeld@gmail.com>
2021-11-08 00:03:47 -0500

    Heroku test for determining email problem

--- Eldridge_Music/settings.py
+++ Eldridge_Music/settings.py
@@ -211,7 +211,8 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-if 'DEVELOPMENT' in os.environ:
+if os.path.exists('env.py'):
+# if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'eldridgemusic@example.com'
 else:
