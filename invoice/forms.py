39e7b0e0934fc74e1d4241452a131e37f7ed3c93
Richardaeld <Richardaeld@gmail.com>
2021-11-12 18:03:36 -0500

    iteration bug test

--- invoice/forms.py
+++ invoice/forms.py
@@ -26,6 +26,7 @@ class InvoiceForm(forms.ModelForm):
             'street_address_shipping': 'Street Address Shipping',
             'state_county': 'State',
             'post_code': 'Post Code',
+            'country': 'Country'
         }
 
         self.fields['name'].widget.attrs['autofocus'] = True
