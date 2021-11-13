26baaf9319ed095ea21caed0c2dcbaab0caa1b0d
Richardaeld <Richardaeld@gmail.com>
2021-11-13 13:04:05 -0500

    More tests for iteration bug

--- invoice/webhook_handler.py
+++ invoice/webhook_handler.py
@@ -101,7 +101,9 @@ class StripeWH_Handler:
                     state_county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_cart=cart,
-                    # stripe_pid=pid,
+                    orginal=True,
+                    # Is causing dupilicate invoices to be created
+                    stripe_pid=pid,
                 )
 
                 invoice_exists = True
