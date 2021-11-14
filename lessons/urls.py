e86d515a077a1fad829152f5ad7ad799b8d36deb
Richardaeld <Richardaeld@gmail.com>
2021-11-13 21:13:00 -0500

    commented out lessons app urls

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.lessons, name='lessons'),
-    path('subscriptions', views.subscriptions, name='subscriptions'),
-    path('details/<sub_id>', views.details, name='details')
+    # path('', views.lessons, name='lessons'),
+    # path('subscriptions', views.subscriptions, name='subscriptions'),
+    # path('details/<sub_id>', views.details, name='details')
 ]
