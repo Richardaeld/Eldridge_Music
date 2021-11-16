e55772a09e58c56ef412532be92648fddb2ad69c
Richardaeld <Richardaeld@gmail.com>
2021-11-15 21:31:50 -0500

    Added MEDIA_URL for blank images

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from django.conf import settings
+
 
 def index(request):
     """ This view returns the index page """
-    return render(request, 'home/index.html')
+    MEDIA_URL = settings.MEDIA_URL
+
+    context = {
+        'MEDIA_URL': MEDIA_URL,
+    }
+
+    return render(request, 'home/index.html', context)
