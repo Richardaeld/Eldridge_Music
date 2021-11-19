08feca923476882522b801843454f59eee095fb6
Richardaeld <Richardaeld@gmail.com>
2021-11-18 21:04:23 -0500

    Added cache ability for S3 bucket, Left commented out until final deployment

--- Eldridge_Music/settings.py
+++ Eldridge_Music/settings.py
@@ -217,6 +217,12 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'USE_AWS' in os.environ:
+    # Cache config
+    # AWS_S3_OBJECT_PARAMETERS = {
+    #     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    #     'CacheControl': 'max-age=94608000',
+    # }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'eldridge-music'
     AWS_S3_REGION_NAME = 'us-east-1'
