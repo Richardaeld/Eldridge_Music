78fdf5d74b80611539e982dc8e7049b6c729993d
Richardaeld <Richardaeld@gmail.com>
2021-11-19 18:31:03 -0500

    Removed debug

--- Eldridge_Music/settings.py
+++ Eldridge_Music/settings.py
@@ -28,11 +28,12 @@ else:
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.path.exists('env.py'):
-    DEBUG = bool(int(os.environ.get("DEVELOPMENT")))
-else:
-    DEBUG = bool(int(os.environ.get("DEVELOPMENT"))) in os.environ
+# if os.path.exists('env.py'):
+#     DEBUG = bool(int(os.environ.get("DEVELOPMENT")))
+# else:
+#     DEBUG = 'DEVELOPMENT' in os.environ
 
+DEBUG = False
 
 ALLOWED_HOSTS = ['eldridge-music.herokuapp.com', '127.0.0.1']
 
