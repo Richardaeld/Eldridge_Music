8717b0af7ec7644981babac8dd937b999ff8a3e9
Richardaeld <Richardaeld@gmail.com>
2021-11-20 02:37:55 -0500

    Added rounded to keep numbers small

--- merchandise/rating.py
+++ merchandise/rating.py
@@ -14,7 +14,7 @@ def add_rating(merch):
         number_of_ratings = len(merch_rating_list)
         if number_of_ratings != 0:
             total_rating = sum(merch_rating_list)
-            avg_rating = total_rating / number_of_ratings
+            avg_rating = round(total_rating / number_of_ratings, 2)
         item.avg_rating = avg_rating
         item.number_of_ratings = number_of_ratings
     return merch
