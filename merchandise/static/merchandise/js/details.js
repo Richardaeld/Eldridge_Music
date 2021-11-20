3c3a3a369666346035a98f1127db7cfa0f474711
Richardaeld <Richardaeld@gmail.com>
2021-11-20 01:52:50 -0500

    Added some form contorl

--- merchandise/static/merchandise/js/details.js
+++ merchandise/static/merchandise/js/details.js
@@ -37,11 +37,30 @@ document.getElementById("in-bag").getElementsByTagName("input")[0].addEventListe
 });
 
 //-------------------- Makes rating input visible and hidden
+//-------------------- Disables button after click to prevent spam
+//-------------------- Adds a max and min limiter
 let rateReveal = document.getElementById("rate-me");
 if (rateReveal != null) {
+    let button = document.getElementById("rate-me-button");
+    let input = document.getElementById("rate-me-input");
+    // Disables button after click to prevent spam
+    button.addEventListener("click", function() {
+        setTimeout(function() {
+            button.setAttribute("disabled", "");
+        },100);
+    });
+
+    // Adds a max and min limiter
+    input.addEventListener("change", function() {
+        if (input.value > 9) {
+            input.value = 9;
+        } else if (input.value < 1) {
+            input.value = 1;
+        }
+    });
+
+    // Makes rating input visible and hidden
     rateReveal.addEventListener("click", function() {
-        let input = document.getElementById("rate-me-input");
-        let button = document.getElementById("rate-me-button");
         if (input.classList.contains("invis")) {
             input.classList.remove("invis");
             button.classList.remove("invis");
@@ -51,3 +70,4 @@ if (rateReveal != null) {
         }
     });
 }
+
